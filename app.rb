525f2c147ba29e458a617d8977e96c2c872ec3d3
Trevor Seitz <trevinla@gmail.com>
2018-05-09 14:36:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  post '/list' do
+    erb :list_puppy
+  end
+
+  get '/create_puppy'
+  erb :create_puppy
 end
