c3a9c503fee2065de4b3083273c084c2e60e8d56
Trevor Seitz <trevinla@gmail.com>
2018-05-09 16:58:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/new' do
+  post '/' do
     # pup = Puppy.new(name, breed, age)
     # @name = params[:name]
     # @breed = params[:breed]
