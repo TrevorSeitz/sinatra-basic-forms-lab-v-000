9779a4b86889a28db52c92ee4891bf7a51f99ea4
Trevor Seitz <trevinla@gmail.com>
2018-05-09 17:17:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,10 +12,10 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
-    # @name = params[:name]
-    # @breed = params[:breed]
-    # @age = params[:age]
+    # @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
 
     erb :display_puppy
   end
