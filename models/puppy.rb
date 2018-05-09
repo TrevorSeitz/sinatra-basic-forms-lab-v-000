a69e6ddb2e791443424cbfdfc4396f88e38796d3
Trevor Seitz <trevinla@gmail.com>
2018-05-09 16:18:11 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,16 +1,11 @@
 class Puppy
-  @@all = []
+  
   attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    @@all << self
-  end
-
-  def all
-    @@all
   end
 
 end
